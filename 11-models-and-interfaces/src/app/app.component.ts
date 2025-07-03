a60a75bb719ed4b5d4577d005fc445cf392af8ec
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-07-03 19:49:40 -0300

    Material Palenight

--- 11-models-and-interfaces/src/app/app.component.ts
+++ 11-models-and-interfaces/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
 import Product from './models/Product';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })

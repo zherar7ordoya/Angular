5fb3a9d3f03be455e958faecfed3105488c785fd
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-07-14 12:51:12 -0300

    Karma

--- 21-testing/src/app/app.component.spec.ts
+++ 21-testing/src/app/app.component.spec.ts
@@ -2,57 +2,50 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [AppComponent],
+        }).compileComponents();
+    });
 
-  it('title should be correct', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    it('Should create the app', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        expect(app).toBeTruthy();
+    });
 
-    const title = fixture.nativeElement.querySelector('h1');
+    it('Title should be correct', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const title = fixture.nativeElement.querySelector('h1');
+        expect(title.textContent).toBe('Testing Angular with Jasmine');
+    });
 
-    expect(title.textContent).toBe('Testing Angular with Jasmine');
-  });
+    it('Form should be invalid if required fields are empty', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        app.movieForm.controls['name'].setValue('');
+        app.movieForm.controls['duration'].setValue('');
+        expect(app.movieForm.valid).toBeFalsy();
+    });
 
-  it('form should be invalid if required fields are empty', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    it('Form should be valid', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        app.movieForm.controls['name'].setValue('Star Wars');
+        app.movieForm.controls['duration'].setValue('200');
+        expect(app.movieForm.valid).toBeTruthy();
+    });
 
-    app.movieForm.controls['name'].setValue('');
-    app.movieForm.controls['duration'].setValue('');
+    it('Button should send the form when clicked', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        const sendBtn = fixture.nativeElement.querySelector('#send-btn');
+        sendBtn.click();
+        expect(app.sentMsg).toBe('Enviado');
 
-    expect(app.movieForm.valid).toBeFalsy();
-  });
-
-  it('form should be valid', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-
-    app.movieForm.controls['name'].setValue('Star Wars');
-    app.movieForm.controls['duration'].setValue('200');
-
-    expect(app.movieForm.valid).toBeTruthy();
-  });
-
-  it('btn should send the form when clicked', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-
-    const sendBtn = fixture.nativeElement.querySelector('#send-btn');
-    sendBtn.click();
-
-    expect(app.sentMsg).toBe('Enviado');
-    
-    // fixture.detectChanges();
-    // const sentMsg = fixture.nativeElement.querySelector("#sent-msg");
-    // expect(sentMsg.textContent).toBe("Enviado");
-  });
+        // fixture.detectChanges();
+        // const sentMsg = fixture.nativeElement.querySelector("#sent-msg");
+        // expect(sentMsg.textContent).toBe("Enviado");
+    });
 });

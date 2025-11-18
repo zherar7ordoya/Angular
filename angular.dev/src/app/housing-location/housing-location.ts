340d140519a3024b13784270a5e5a30710b07383
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-11-18 12:58:19 -0300

    Initialize Angular app with core files and config
    Add initial Angular application files, including core and platform-browser dependencies, configuration files, assets, and source code for main app and components. This sets up the project structure and dependencies for development.

---
+++ angular.dev/src/app/housing-location/housing-location.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-housing-location",
+    imports: [],
+    template: ` <p>housing-location works!</p> `,
+    styleUrls: ["./housing-location.css"],
+})
+export class HousingLocation {}
